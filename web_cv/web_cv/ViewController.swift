9024363bff8f247968c3e3272a88ed48e38844a1
marouane195 <marouane@rouibah.fr>
2017-10-01 15:44:34 +0200

    app web_cv premiere page

---
+++ web_cv/web_cv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web_cv
+//
+//  Created by rouibah on 01/10/2017.
+//  Copyright © 2017 rouibah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
